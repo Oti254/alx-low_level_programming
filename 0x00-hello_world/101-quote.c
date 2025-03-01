a590a37263a47560587c23be4b3a865222048c86
Oti254 <otrated555@gmail.com>
2025-03-01 10:10:55 +0300

    strlen not allowed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 
 /**
  * main - prints to stderr
@@ -12,6 +11,6 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str, strlen(str));
+	write(2, str, sizeof(str) - 1);
 	return (1);
 }
