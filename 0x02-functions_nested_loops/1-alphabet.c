8272866be3cef5717e69c737fe4f23c1dd00f6ca
Oti254 <otrated555@gmail.com>
2025-03-04 23:49:38 +0300

    betty code style corrections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,15 @@
 #include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ *
+ * Return: Nothing
+ *
+ */
 void print_alphabet(void)
 {
 
 	char a;
-	
+
 	a = 97;
 	while (a < 123)
 	{
