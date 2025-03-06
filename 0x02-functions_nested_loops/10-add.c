1ef8ba2242e06f256d68b88d35d7102feb3b274c
Oti254 <otrated555@gmail.com>
2025-03-06 17:53:30 +0300

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - adds to integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: result from a + b
+ *
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = (a + b);
+	return (sum);
+}
