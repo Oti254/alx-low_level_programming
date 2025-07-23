7acb86ccf6c568cfd49b625945d502a4abd85378
Oti254 <otrated555@gmail.com>
2025-07-23 17:27:35 +0300

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - returns length of a string
+ *
+ * @s: string evaluated
+ *
+ * Return: Integer number length
+ *
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
